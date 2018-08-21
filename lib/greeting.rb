fb723af7966498777eb50f8061d5c61d79b02e56
Yu Li <yli0607x@gmail.com>
2018-08-21 19:51:47 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,6 @@
 puts "Hi! I'm Hal, what's your name?"
-
+def greeting(name)
   name = gets.strip
   puts "Hello, #{name}. It's nice to meet you."
+  greeting(name)
 end# code the #greeting method here!
